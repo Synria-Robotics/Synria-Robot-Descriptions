ef3d07e30d80836b40b7b9842cd2b1afe294e8a4
toko-xuanya <tech@xuanyatech.com>
2026-01-19 22:36:52 +0800

    Update Bessica-M v1.0

--- synriard/mjcf/Bessica_D_v1_1/__init__.py
+++ synriard/mjcf/Bessica_D_v1_1/__init__.py
@@ -6,8 +6,8 @@ _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 
-Bessica_D_v1_1_skeleton = SimpleNamespace()
-Bessica_D_v1_1_skeleton.xml = os.path.join(_MODULE_PATH, "Bessica_D_v1_1_skeleton.xml")
+Bessica_D_v1_1_covered = SimpleNamespace()
+Bessica_D_v1_1_covered.xml = os.path.join(_MODULE_PATH, "Bessica_D_v1_1_covered.xml")
 
-Bessica_D_v1_1_skeleton_interactive = SimpleNamespace()
-Bessica_D_v1_1_skeleton_interactive.xml = os.path.join(_MODULE_PATH, "Bessica_D_v1_1_skeleton_interactive.xml")
+Bessica_D_v1_1_covered_interactive = SimpleNamespace()
+Bessica_D_v1_1_covered_interactive.xml = os.path.join(_MODULE_PATH, "Bessica_D_v1_1_covered_interactive.xml")
