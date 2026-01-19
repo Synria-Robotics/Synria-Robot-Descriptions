eada48ddcc35e61208d6af2a1a7a170c142ed937
toko-xuanya <tech@xuanyatech.com>
2026-01-19 19:27:43 +0800

    Change black color to 0.15 0.15 0.15 1

--- synriard/urdf/__init__.py
+++ synriard/urdf/__init__.py
@@ -1,7 +1,6 @@
 # This line makes the sub-folders available as attributes of this module
 from . import Alicia_D_v5_5
 from . import Alicia_D_v5_6
-from . import Alicia_M_v1_0
 from . import Alicia_M_v1_1
 from . import Bessica_D_v1_0
 from . import Bessica_D_v1_1
@@ -15,7 +14,6 @@ from . import Bessica_M_v1_0
 __all__ = [
     "Alicia_D_v5_5",
     "Alicia_D_v5_6",
-    "Alicia_M_v1_0",
     "Alicia_M_v1_1",
     "Bessica_D_v1_0",
     "Bessica_D_v1_1",
