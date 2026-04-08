275824a12952a219abd5bed557a970a063d0d60e
programme184 <1840811549@qq.com>
2026-04-08 19:31:17 +0800

    Add bimanual alicia m

--- synriard/mjcf/Alicia_M_v1_1/__init__.py
+++ synriard/mjcf/Alicia_M_v1_1/__init__.py
@@ -8,3 +8,6 @@ _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 Alicia_M_v1_1_follower = SimpleNamespace()
 Alicia_M_v1_1_follower.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_follower.xml")
+
+Alicia_M_v1_1_bimanual_interactive = SimpleNamespace()
+Alicia_M_v1_1_bimanual_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bimanual_interactive.xml")
