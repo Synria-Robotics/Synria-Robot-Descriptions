a539a8b92b4d6def62d54e036b0c15fe765d67b4
Yitao YANG <yangyitao200107@outlook.com>
2026-05-18 22:37:37 +0800

    Add Alicia_M v1.2 to module imports and __all__ list

--- synriard/urdf/__init__.py
+++ synriard/urdf/__init__.py
@@ -2,6 +2,7 @@
 from . import Alicia_D_v5_6
 from . import Alicia_M_v1_0
 from . import Alicia_M_v1_1
+from . import Alicia_M_v1_2
 from . import Bessica_D_v1_1
 from . import Bessica_M_v1_0
 # Note: Do not import specific URDF namespace objects here (e.g., Bessica_D_v1_0_Covered),
@@ -14,6 +15,7 @@ __all__ = [
     "Alicia_D_v5_6",
     "Alicia_M_v1_0",
     "Alicia_M_v1_1",
+    "Alicia_M_v1_2",
     "Bessica_D_v1_1",
     "Bessica_M_v1_0",
 ]
