a52034a0e0edef7a26762f7b91c865f163a71d45
programme184 <1840811549@qq.com>
2026-06-01 11:55:36 +0800

    Fix xml link name mismatch

--- synriard/mjcf/Alicia_M_v1_1/__init__.py
+++ synriard/mjcf/Alicia_M_v1_1/__init__.py
@@ -5,13 +5,19 @@ from types import SimpleNamespace
 _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 Alicia_M_v1_1_bi_interactive = SimpleNamespace()
-Alicia_M_v1_1_bi_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bi_interactive.xml")
+Alicia_M_v1_1_bi_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bi_vertical_interactive.xml")
 
 Alicia_M_v1_1_bimanual_interactive = SimpleNamespace()
-Alicia_M_v1_1_bimanual_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bimanual_interactive.xml")
+Alicia_M_v1_1_bimanual_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bi_vertical_interactive.xml")
+
+Alicia_M_v1_1_bi_vertical_interactive = SimpleNamespace()
+Alicia_M_v1_1_bi_vertical_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_bi_vertical_interactive.xml")
 
 Alicia_M_v1_1_follower = SimpleNamespace()
 Alicia_M_v1_1_follower.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_follower.xml")
+
+Alicia_M_v1_1_follower_interactive = SimpleNamespace()
+Alicia_M_v1_1_follower_interactive.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_follower_interactive.xml")
 
 Alicia_M_v1_1_vertical = SimpleNamespace()
 Alicia_M_v1_1_vertical.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_1_vertical.xml")
