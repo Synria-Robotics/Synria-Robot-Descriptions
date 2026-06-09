bacec0fd39c3818dc2ce7d7c8073a282dee99b67
programme184 <1840811549@qq.com>
2026-06-09 17:23:21 +0800

    Add arx alicia m

--- synriard/mjcf/Alicia_M_v1_2/__init__.py
+++ synriard/mjcf/Alicia_M_v1_2/__init__.py
@@ -6,3 +6,6 @@ _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 Alicia_M_v1_2_follower = SimpleNamespace()
 Alicia_M_v1_2_follower.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower.xml")
+
+Alicia_M_v1_2_follower_ARX = SimpleNamespace()
+Alicia_M_v1_2_follower_ARX.xml = os.path.join(_MODULE_PATH, "Alicia_M_1_2_follower_ARX.xml")
