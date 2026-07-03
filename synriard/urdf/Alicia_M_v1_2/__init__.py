33af8c43c1f1844e11839dd2e1ae0c255d38dc1b
programme184 <1840811549@qq.com>
2026-07-03 10:47:53 +0800

    Update ARX joint direction

--- synriard/urdf/Alicia_M_v1_2/__init__.py
+++ synriard/urdf/Alicia_M_v1_2/__init__.py
@@ -4,8 +4,8 @@ from types import SimpleNamespace
 # Get the absolute path to THIS directory
 _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-Alicia_M_1_2_follower_ARX = SimpleNamespace()
-Alicia_M_1_2_follower_ARX.urdf = os.path.join(_MODULE_PATH, "Alicia_M_1_2_follower_ARX.urdf")
+Alicia_M_v1_2_follower_ARX = SimpleNamespace()
+Alicia_M_v1_2_follower_ARX.urdf = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower_ARX.urdf")
 
 Alicia_M_v1_2_follower = SimpleNamespace()
 Alicia_M_v1_2_follower.urdf = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower.urdf")
