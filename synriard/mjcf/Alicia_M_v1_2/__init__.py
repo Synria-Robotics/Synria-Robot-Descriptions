054e00e43833adffaa730d9430532b4ec85811d8
programme184 <1840811549@qq.com>
2026-08-17 17:27:15 +0800

    Add arx with paxini sensor

--- synriard/mjcf/Alicia_M_v1_2/__init__.py
+++ synriard/mjcf/Alicia_M_v1_2/__init__.py
@@ -7,5 +7,8 @@ _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 Alicia_M_v1_2_follower_ARX = SimpleNamespace()
 Alicia_M_v1_2_follower_ARX.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower_ARX.xml")
 
+Alicia_M_v1_2_follower_ARX_PX6D = SimpleNamespace()
+Alicia_M_v1_2_follower_ARX_PX6D.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower_ARX_PX6D.xml")
+
 Alicia_M_v1_2_follower = SimpleNamespace()
 Alicia_M_v1_2_follower.xml = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower.xml")
--- synriard/urdf/Alicia_M_v1_2/__init__.py
+++ synriard/urdf/Alicia_M_v1_2/__init__.py
@@ -7,5 +7,8 @@ _MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 Alicia_M_v1_2_follower_ARX = SimpleNamespace()
 Alicia_M_v1_2_follower_ARX.urdf = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower_ARX.urdf")
 
+Alicia_M_v1_2_follower_ARX_PX6D = SimpleNamespace()
+Alicia_M_v1_2_follower_ARX_PX6D.urdf = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower_ARX_PX6D.urdf")
+
 Alicia_M_v1_2_follower = SimpleNamespace()
 Alicia_M_v1_2_follower.urdf = os.path.join(_MODULE_PATH, "Alicia_M_v1_2_follower.urdf")
